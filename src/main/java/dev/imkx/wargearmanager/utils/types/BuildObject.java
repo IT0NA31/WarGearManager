39dd8afc5469ee750631387cae2b8884ce631e1f
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-10-17 21:32:30 +0200

    added offsets to BuildObject

--- src/main/java/dev/imkx/wargearmanager/utils/types/BuildObject.java
+++ src/main/java/dev/imkx/wargearmanager/utils/types/BuildObject.java
@@ -1,6 +1,7 @@
 package dev.imkx.wargearmanager.utils.types;
 
 import org.bukkit.Location;
+import org.bukkit.util.Vector;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -10,6 +11,42 @@ public class BuildObject implements Serializable {
 
 	public void addPart(Part part) {
 		parts.add(part);
+	}
+
+	public Vector getOffsetStart() {
+		Vector offsetStart = null;
+
+		for(Part part : parts) {
+			if(offsetStart == null) {
+				offsetStart = part.getOffsetStart();
+			} else {
+				Vector partOffsetStart = part.getOffsetStart();
+
+				offsetStart.setX(Math.min(offsetStart.getX(), partOffsetStart.getX()));
+				offsetStart.setY(Math.min(offsetStart.getY(), partOffsetStart.getY()));
+				offsetStart.setZ(Math.min(offsetStart.getZ(), partOffsetStart.getZ()));
+			}
+		}
+
+		return offsetStart;
+	}
+
+	public Vector getOffsetEnd() {
+		Vector offsetEnd = null;
+
+		for(Part part : parts) {
+			if(offsetEnd == null) {
+				offsetEnd = part.getOffsetEnd();
+			} else {
+				Vector partOffsetEnd = part.getOffsetEnd();
+
+				offsetEnd.setX(Math.max(offsetEnd.getX(), partOffsetEnd.getX()));
+				offsetEnd.setY(Math.max(offsetEnd.getY(), partOffsetEnd.getY()));
+				offsetEnd.setZ(Math.max(offsetEnd.getZ(), partOffsetEnd.getZ()));
+			}
+		}
+
+		return offsetEnd;
 	}
 
 	public void build(Location location) {
