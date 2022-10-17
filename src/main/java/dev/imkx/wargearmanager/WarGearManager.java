9be13734ea6a88aa4eb869536fe09bc06b44c5b5
IT0NA31 <ITnullNA31@gmail.com>
2022-10-17 18:19:57 +0200

    implemented command wargear

--- src/main/java/dev/imkx/wargearmanager/WarGearManager.java
+++ src/main/java/dev/imkx/wargearmanager/WarGearManager.java
@@ -1,5 +1,6 @@
 package dev.imkx.wargearmanager;
 
+import dev.imkx.wargearmanager.commands.WarGearCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WarGearManager extends JavaPlugin {
@@ -7,6 +8,7 @@ public final class WarGearManager extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
+		getCommand("wargear").setExecutor(new WarGearCommand());
 
 	}
 
---
+++ src/main/java/dev/imkx/wargearmanager/commands/WarGearCommand.java
@@ -0,0 +1,13 @@
+package dev.imkx.wargearmanager.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class WarGearCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
+
+        return true;
+    }
+}
