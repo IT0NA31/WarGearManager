185400e8f8c40944780a1a4b8813e4bf149eb935
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-10-17 17:51:05 +0200

    added basic workspace files

---
+++ src/main/java/dev/imkx/wargearmanager/WarGearManager.java
@@ -0,0 +1,17 @@
+package dev.imkx.wargearmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarGearManager extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
