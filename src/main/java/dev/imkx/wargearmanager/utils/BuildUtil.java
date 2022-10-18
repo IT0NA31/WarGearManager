41e180b3a2571bf8a3fc960755b92b3dcef55da9
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-10-18 13:27:46 +0200

    close io streams

--- src/main/java/dev/imkx/wargearmanager/utils/BuildUtil.java
+++ src/main/java/dev/imkx/wargearmanager/utils/BuildUtil.java
@@ -6,21 +6,29 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class BuildUtil {
 	public static BuildObject fromBuildResource(String resourceName) throws IOException, ClassNotFoundException {
 		ObjectInputStream is = new ObjectInputStream(BuildUtil.class.getClassLoader().getResourceAsStream(resourceName));
-		return (BuildObject) is.readObject();
+		BuildObject buildObject = (BuildObject) is.readObject();
+		is.close();
+		return buildObject;
 	}
 
 	public static BuildObject fromBuildFile(String filePath) throws IOException, ClassNotFoundException {
 		ObjectInputStream is = new ObjectInputStream(Files.newInputStream(Paths.get(filePath)));
-		return (BuildObject) is.readObject();
+		BuildObject buildObject = (BuildObject) is.readObject();
+		is.close();
+		return buildObject;
 	}
 
 	public static void toBuildFile(String filePath, BuildObject buildObject) throws IOException {
-		ObjectOutputStream os = new ObjectOutputStream(Files.newOutputStream(Paths.get(filePath)));
+		Path path = Paths.get(filePath);
+		Files.createDirectories(path.getParent());
+		ObjectOutputStream os = new ObjectOutputStream(Files.newOutputStream(path));
 		os.writeObject(buildObject);
+		os.close();
 	}
 }
