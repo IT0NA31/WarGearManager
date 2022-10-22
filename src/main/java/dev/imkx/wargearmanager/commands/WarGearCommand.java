7df63885b766f1c0e7c9e9cc058dd5747940314e
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-10-22 16:51:44 +0200

    added more commands and cleaned up

--- src/main/java/dev/imkx/wargearmanager/commands/WarGearCommand.java
+++ src/main/java/dev/imkx/wargearmanager/commands/WarGearCommand.java
@@ -1,6 +1,7 @@
 package dev.imkx.wargearmanager.commands;
 
 import dev.imkx.wargearmanager.WarGearManager;
+import dev.imkx.wargearmanager.utils.BuildUtil;
 import dev.imkx.wargearmanager.utils.ParticleUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,26 +10,52 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class WarGearCommand implements CommandExecutor {
     HashMap<UUID, BukkitTask> tasks = new HashMap<>();
 
+    private void removeTask(UUID uuid) {
+        if (tasks.containsKey(uuid)) {
+            tasks.get(uuid).cancel();
+            tasks.remove(uuid);
+        }
+    }
+
     @Override
     public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
         if(s instanceof ConsoleCommandSender)
             return true;
         Player p = (Player) s;
-        if (args.length > 0 && args[0].equalsIgnoreCase("show")) {
-            BukkitTask task = new ParticleUtil.BuildObjectOutliner(p, WarGearManager.getBasicWarGearArena())
-                    .runTaskTimer(WarGearManager.getInstance(), 0, 10);
-            tasks.put(p.getUniqueId(), task);
-        } else if (args.length > 0 && args[0].equalsIgnoreCase("hide")) {
-            if(tasks.containsKey(p.getUniqueId())) {
-                tasks.get(p.getUniqueId()).cancel();
-                tasks.remove(p.getUniqueId());
-            }
+        BukkitTask task;
+        switch (args[0]) {
+            case "show":
+                removeTask(p.getUniqueId());
+                task = new ParticleUtil.BuildObjectOutliner(p, WarGearManager.getBasicWarGearArena())
+                        .runTaskTimer(WarGearManager.getInstance(), 0, 10);
+                tasks.put(p.getUniqueId(), task);
+                break;
+            case "hide":
+                removeTask(p.getUniqueId());
+                break;
+            case "freeze":
+                removeTask(p.getUniqueId());
+                task = new ParticleUtil.BuildObjectOutliner(p, WarGearManager.getBasicWarGearArena(), p.getLocation())
+                        .runTaskTimer(WarGearManager.getInstance(), 0, 10);
+                tasks.put(p.getUniqueId(), task);
+                break;
+            case "apply":
+                WarGearManager.getBasicWarGearArena().build(p.getLocation());
+                break;
+            case "save":
+                try {
+                    BuildUtil.toBuildFile("plugins/WarGearManager/basicWarGearArena.bf", WarGearManager.getBasicWarGearArena());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
         }
         return true;
     }
